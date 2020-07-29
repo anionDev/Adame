b950ecd0359168bae3a8cd00d6b55badc7b2d851
Marius Göcke <marius.goecke@gmail.com>
2020-07-29 20:28:14 +0200

    little updates/renamings/formattings

--- src/adame/core.py
+++ src/adame/core.py
@@ -1,14 +1,16 @@
 import argparse
 from argparse import RawTextHelpFormatter
 import sys
-version="0.1.0"
+version = "0.1.0"
+
 
 class AdameCore(object):
-    verbose=False
+    verbose = False
+
     def __init__(self):
         pass
 
-    def create_new_environment(self, name: str, folder: str, image:str):
+    def create_new_environment(self, name: str, folder: str, image: str):
         print("Hello from Adame: create_new_environment. This function is not implemented yet. name: "+name+",  folder: "+folder+",  image: "+image)
 
     def start_environment(self, configuration_file: str):
@@ -18,26 +20,27 @@ class AdameCore(object):
         print("Hello from Adame: stop_environment. This function is not implemented yet. file: "+configuration_file)
 
 
-def create_new_environment(name: str, folder: str, image:str,verbose:bool):
-    core=AdameCore()
-    core.verbose=verbose
-    core.create_new_environment(name, folder,image)
+def create_new_environment(name: str, folder: str, image: str, verbose: bool):
+    core = AdameCore()
+    core.verbose = verbose
+    core.create_new_environment(name, folder, image)
 
 
-def start_environment(configuration_file,verbose:bool):
-    core=AdameCore()
-    core.verbose=verbose
-    core.AdameCore().start_environment(configuration_file)
+def start_environment(configuration_file, verbose: bool):
+    core = AdameCore()
+    core.verbose = verbose
+    core.start_environment(configuration_file)
 
 
-def stop_environment(configuration_file,verbose:bool):
-    core=AdameCore()
-    core.verbose=verbose
-    core.AdameCore().stop_environment(configuration_file)
+def stop_environment(configuration_file, verbose: bool):
+    core = AdameCore()
+    core.verbose = verbose
+    core.stop_environment(configuration_file)
 
 
 def get_adame_version():
     return version
+
 
 def adame_cli():
     arger = argparse.ArgumentParser(description="""Adame
@@ -54,31 +57,31 @@ Requirements:
 
     subparsers = arger.add_subparsers(dest="command")
 
-    create_command_name="create"
+    create_command_name = "create"
     create_parser = subparsers.add_parser(create_command_name)
     create_parser.add_argument("name")
     create_parser.add_argument("folder")
     create_parser.add_argument("image")
 
-    start_command_name="start"
+    start_command_name = "start"
     start_parser = subparsers.add_parser(start_command_name)
     start_parser.add_argument("configurationfile")
 
-    stop_command_name="stop"
+    stop_command_name = "stop"
     stop_parser = subparsers.add_parser(stop_command_name)
     stop_parser.add_argument("configurationfile")
 
-    options=arger.parse_args()
-    verbose=options.v
+    options = arger.parse_args()
+    verbose = options.v
     if options.command == create_command_name:
-        create_new_environment(options.name,options.folder,options.image,verbose)
+        create_new_environment(options.name, options.folder, options.image, verbose)
     elif options.command == start_command_name:
-        start_environment(options.configurationfile,verbose)
+        start_environment(options.configurationfile, verbose)
     elif options.command == stop_command_name:
-        stop_environment(options.configurationfile,verbose)
+        stop_environment(options.configurationfile, verbose)
     else:
-        if options.command==None:
-            print("Adame "+get_adame_version())
+        if options.command == None:
+            print("Adame " + get_adame_version())
             print('Enter a command or run "adame --help" to get help about the usage')
         else:
             print("Unknown command: "+options.command)
--- src/adameTests/tests.py
+++ src/adameTests/tests.py
@@ -1,13 +1,10 @@
-from adame import adame
-import sys
-sys.path.append("..")
+from Adame.core import *
 
 
 def test_1():
-    assert adame.get_adame_version() == "0.1.0"
+    assert adame.get_adame_version() == "0.1.1"
 
 
 def test_2():
-    adame.create_new_environment("name", "repository_folder","image",False)
+    adame.create_new_environment("name", "repository_folder", "image", False)
     assert 1 == 1
-
