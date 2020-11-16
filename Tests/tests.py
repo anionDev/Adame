edbd575c5a1639f1b7077f725f15ff45e8701625
Marius Göcke <marius.goecke@gmail.com>
2020-11-16 14:26:51 +0100

    fixed stop-test

--- Tests/tests.py
+++ Tests/tests.py
@@ -26,6 +26,8 @@ class EnvironmentForTest:
         assert self.adame.create("myapplication", self.folder, "httpd:latest", "owner") == 0
         assert not self.adame._private_container_is_running()
         self.adame._private_sc.mock_program_calls = True
+        self.adame.verify_no_pending_mock_process_queries()
+        self.adame._private_sc.verify_no_pending_mock_program_calls()
 
     def dispose(self):
         ensure_directory_does_not_exist(self.folder)
@@ -114,8 +116,35 @@ class MiscellaneousTests(unittest.TestCase):
 
             environment_for_test = EnvironmentForTest()
             environment_for_test.create()
-            assert environment_for_test.adame.start(environment_for_test.adame_configuration_file) == 0
+
+            environment_for_test.adame._private_sc.register_mock_program_call("docker-compose",  re.escape("up --build --quiet-pull --remove-orphans --force-recreate --always-recreate-deps"), re.escape(environment_for_test.adame._private_configuration_folder), 0, "", "", 40)
+            environment_for_test.adame._private_sc.register_mock_program_call("snort", re.escape(f'-c "{environment_for_test.adame._private_networktrafficgeneratedrules_file}" -l "{environment_for_test.adame._private_log_folder_for_intrusiondetectionsystem}"'), "", 0, "", "", 44)
+            environment_for_test.adame._private_sc.register_mock_program_call("git", "reset", re.escape(environment_for_test.adame._private_repository_folder), 0, "", "", 40)
+            environment_for_test.adame._private_sc.register_mock_program_call("git", f'stage -- "{re.escape(environment_for_test.adame._private_running_information_file)}"', re.escape(environment_for_test.adame._private_repository_folder), 0, "", "", 48)
+            environment_for_test.adame._private_sc.register_mock_program_call("git", "diff", re.escape(environment_for_test.adame._private_repository_folder), 0, "(some diff content)", "", 52)
+            environment_for_test.adame._private_sc.register_mock_program_call("git", re.escape('commit --message="Started container (Container-process: 40; IDS-process: 44)" --author="Adame <>"'), re.escape(environment_for_test.adame._private_repository_folder), 0, "", "", 56)
+            environment_for_test.adame._private_sc.register_mock_program_call("git", re.escape('rev-parse --verify HEAD'), re.escape(environment_for_test.adame._private_repository_folder), 0, "3c5a38ad96d0acf5e2822bbcd655387b42352cb0", "", 60)
+            assert environment_for_test.adame.start(environment_for_test.adame_configuration_file)==0
+
+            environment_for_test.adame.register_mock_process_query(40, "docker-compose")
             assert environment_for_test.adame._private_container_is_running()
+
+            environment_for_test.adame.register_mock_process_query(44, "snort")
+            assert environment_for_test.adame._private_intrusion_detection_is_running()
+
+            environment_for_test.adame.verify_no_pending_mock_process_queries()
+            environment_for_test.adame._private_sc.verify_no_pending_mock_program_calls()
+
+            environment_for_test.adame._private_sc.register_mock_program_call("git", "reset", re.escape(environment_for_test.adame._private_repository_folder), 0, "", "", 40)
+            environment_for_test.adame._private_sc.register_mock_program_call("git", f'stage -- "{re.escape(environment_for_test.adame._private_running_information_file)}"', re.escape(environment_for_test.adame._private_repository_folder), 0, "", "", 48)
+            environment_for_test.adame._private_sc.register_mock_program_call("git", "diff", re.escape(environment_for_test.adame._private_repository_folder), 0, "(some diff content)", "", 52)
+            environment_for_test.adame._private_sc.register_mock_program_call("git", re.escape('commit --message="Stopped container (Container-process: ; IDS-process: )" --author="Adame <>"'), re.escape(environment_for_test.adame._private_repository_folder), 0, "", "", 56)
+            environment_for_test.adame._private_sc.register_mock_program_call("git", re.escape('rev-parse --verify HEAD'), re.escape(environment_for_test.adame._private_repository_folder), 0, "4d6a38ad96d0acf5e2822bbcd655387b42352cc1", "", 60)
+
+            environment_for_test.adame._private_sc.register_mock_program_call("docker-compose",  re.escape("down --remove-orphans"), re.escape(environment_for_test.adame._private_configuration_folder), 0, "", "", 68)
+            environment_for_test.adame._private_sc.register_mock_program_call("kill",  re.escape("44"), "", 0, "", "", 72)
+            environment_for_test.adame.register_mock_process_query(40, "docker-compose")
+            environment_for_test.adame.register_mock_process_query(44, "snort")
 
             # act
 
