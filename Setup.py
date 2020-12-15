61c321b8f5b07ce68ac701599c25ee3293d202d4
Marius Göcke <marius.goecke@gmail.com>
2020-12-15 17:02:46 +0100

    updated sc to 2.0.18

--- Setup.py
+++ Setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
         "netifaces>=0.10.9",
         "psutil>=5.7.3",
-        "ScriptCollection>=2.0.17",
+        "ScriptCollection>=2.0.18",
     ],
     entry_points={
         'console_scripts': [
