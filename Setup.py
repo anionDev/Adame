fe3f242433deb2d7b72755f6e9561eda9c55714b
Marius Göcke <marius.goecke@gmail.com>
2021-07-13 10:09:33 +0200

    updated scriptcollection

--- Setup.py
+++ Setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
         "netifaces==0.10.9",
         "psutil==5.7.3",
-        "ScriptCollection==2.5.25",
+        "ScriptCollection==2.5.26",
     ],
     entry_points={
         'console_scripts': [
