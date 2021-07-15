6060eea4d91938ea3f9756a45b2e278ba238888b
Marius Göcke <marius.goecke@gmail.com>
2021-07-15 20:07:23 +0200

    added packaging-dependency

--- Setup.py
+++ Setup.py
@@ -32,6 +32,7 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=[
         "netifaces==0.10.9",
+        "packaging==21.0",
         "psutil==5.7.3",
         "ScriptCollection==2.5.27",
     ],
