68da130f1d699321ad5838bb92a50a448d3c8342
Marius Göcke <marius.goecke@gmail.com>
2022-10-22 20:51:47 +0200

    update

--- Adame/Other/CommonTasks.py
+++ Adame/Other/CommonTasks.py
@@ -12,7 +12,7 @@ def common_tasks():
     sc = ScriptCollectionCore()
     version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path("../..", os.path.dirname(file)))
     sc.replace_version_in_pyproject_file(GeneralUtilities.resolve_relative_path("../pyproject.toml", folder_of_current_file), version)
-    sc.replace_version_in_python_file(GeneralUtilities.resolve_relative_path("../Adame/AdameCore.py", folder_of_current_file), version)
+    sc.replace_version_in_python_file(GeneralUtilities.resolve_relative_path("../Adame/Adame.py", folder_of_current_file), version)
     tfcps = TasksForCommonProjectStructure()
     tfcps.standardized_tasks_do_common_tasks(file, 1, "QualityCheck", True, sys.argv)
 
