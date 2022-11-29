03b18260b6632a241376fbad48274a1bf3f44199
Marius Göcke <marius.goecke@gmail.com>
2022-11-29 11:26:13 +0100

    updated

--- Adame/Other/CommonTasks.py
+++ Adame/Other/CommonTasks.py
@@ -10,11 +10,12 @@ def common_tasks():
     file = str(Path(__file__).absolute())
     folder_of_current_file = os.path.dirname(file)
     sc = ScriptCollectionCore()
-    version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path("../..", os.path.dirname(file)))
-    sc.replace_version_in_ini_file(GeneralUtilities.resolve_relative_path("../pyproject.toml", folder_of_current_file), version)
+    version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path("../..", os.path.dirname(file)))  # Should always be the same as the project-version
+    sc.replace_version_in_ini_file(GeneralUtilities.resolve_relative_path("../setup.cfg", folder_of_current_file), version)
     sc.replace_version_in_python_file(GeneralUtilities.resolve_relative_path("../Adame/Adame.py", folder_of_current_file), version)
-    tfcps = TasksForCommonProjectStructure()
-    tfcps.standardized_tasks_do_common_tasks(file, version, 1, "QualityCheck", True, None, sys.argv)
+    t = TasksForCommonProjectStructure()
+    additionalargumentsfile_from_commandline_arguments = t.get_additionalargumentsfile_from_commandline_arguments(sys.argv, None)
+    t.standardized_tasks_do_common_tasks(file, version, 1, "QualityCheck", True, additionalargumentsfile_from_commandline_arguments, sys.argv)
 
 
 if __name__ == "__main__":
--- Adame/ReadMe.md
+++ Adame/ReadMe.md
@@ -2,6 +2,8 @@
 
 ![Development-state](https://img.shields.io/badge/development--state-maintenance%20updates%20only-green)
 
+## Purpose
+
 Adame (pronounced: `ăˈ.dam`) is the abbreviation for "Automatic Docker Application Management Engine". So in short: Adame is a tool to manage docker-applications as part in a Linux-environment.
 
 ## License
