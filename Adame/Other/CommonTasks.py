17d621504302a00d82d2f0cd5362324e23b3ace5
Marius Göcke <marius.goecke@gmail.com>
2023-03-07 23:18:55 +0100

    updated

--- Adame/Other/CommonTasks.py
+++ Adame/Other/CommonTasks.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import re
 from pathlib import Path
 from ScriptCollection.GeneralUtilities import GeneralUtilities
 from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
@@ -10,12 +11,17 @@ def common_tasks():
     file = str(Path(__file__).absolute())
     folder_of_current_file = os.path.dirname(file)
     sc = ScriptCollectionCore()
-    version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path("../..", os.path.dirname(file)))  # Should always be the same as the project-version
-    sc.replace_version_in_ini_file(GeneralUtilities.resolve_relative_path("../setup.cfg", folder_of_current_file), version)
-    sc.replace_version_in_python_file(GeneralUtilities.resolve_relative_path("../Adame/Adame.py", folder_of_current_file), version)
+    cmd_args = sys.argv
     t = TasksForCommonProjectStructure()
-    additionalargumentsfile_from_commandline_arguments = t.get_additionalargumentsfile_from_commandline_arguments(sys.argv, None)
-    t.standardized_tasks_do_common_tasks(file, version, 1, "QualityCheck", True, additionalargumentsfile_from_commandline_arguments, sys.argv)
+    codeunitname = os.path.basename(GeneralUtilities.resolve_relative_path("..", os.path.dirname(file)))
+    verbosity = t.get_verbosity_from_commandline_arguments(cmd_args, 1)
+    targetenvironmenttype = t.get_targetenvironmenttype_from_commandline_arguments(cmd_args, "QualityCheck")
+    additional_arguments_file = t.get_additionalargumentsfile_from_commandline_arguments(cmd_args, None)
+    codeunit_version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path(
+        "../..", os.path.dirname(file)))  # Should always be the same as the project-version
+    sc.replace_version_in_ini_file(GeneralUtilities.resolve_relative_path("../setup.cfg", folder_of_current_file), codeunit_version)
+    sc.replace_version_in_python_file(GeneralUtilities.resolve_relative_path(f"../{codeunitname}/{codeunitname}.py", folder_of_current_file), codeunit_version)
+    t.standardized_tasks_do_common_tasks(file, codeunit_version, verbosity, targetenvironmenttype, True, additional_arguments_file, False, cmd_args)
 
 
 if __name__ == "__main__":
