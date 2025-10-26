01cf8f690ce93864427a7ac3e1eb9fa19ad5792c
Marius Göcke <marius.goecke@gmail.com>
2025-10-26 11:23:34 +0100

    Updates due to changed dependencies

--- Adame/AdameTests/EnvironmentForTest.py
+++ Adame/AdameTests/EnvironmentForTest.py
@@ -2,7 +2,7 @@ import tempfile
 import uuid
 import os
 from ScriptCollection.GeneralUtilities import GeneralUtilities
-from ..Adame.Adame import Adame
+from ..Adame.AdameCore import Adame
 
 
 class EnvironmentForTest:
